95558715df0f4c64bc45ae43d5d516bfea327117
DevArslan <arslan.gareev.2017@gmail.com>
2020-08-08 01:59:13 +0300

    fix: CORS, feat: add POST method

--- server.js
+++ server.js
@@ -15,8 +15,19 @@ socket.on('connection', (socket) => {
     console.log('user connected',socket.id)
 })
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.get('/rooms', (req,res)=>{
     res.json(rooms)
+});
+
+app.post('/rooms', (req,res)=>{
+    console.log(res)
+    res.send()
 });
 
 server.listen(5050,(error)=>{
