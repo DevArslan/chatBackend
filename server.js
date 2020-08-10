c0eeced001c802da77b54144730593321ee7579b
DevArslan <arslan.gareev.2017@gmail.com>
2020-08-10 20:30:57 +0300

    delete: node modules from repo

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const socket = require('socket.io')(server);
 
 // let rooms = new Map
 let rooms = { '1': { 'users': [{ '1': '52352' }, { '3': '5274352' }, { '2': '5232352' }], 'messages': [] }, '2': { 'users': [{ '1': '52352' }, { '3': '5274352' }, { '2': '5232352' }], 'messages': [] } }
+let roomID
 
 socket.on('connection', (io) => {
-    let roomID
+    // let roomID
     io.on('JOIN', (data) => {
         roomID = data.id
         io.join(data.id)
@@ -31,21 +32,49 @@ socket.on('connection', (io) => {
             rooms[roomObject.id].users = users
         }
         const currentRoomUsers = rooms[roomObject.id].users
-
         io.to(roomObject.id).broadcast.emit('USERS', currentRoomUsers)
     })
 
-    io.on('disconnect', () => {
+    io.on('POST_MESSAGE', (data) => {
+        console.log(data)
+        const roomID = data.id
+        const username = data.username
+        const message = data.message
+        const date = data.date
+        
+        const messageObject = {username, message, date}
+
+
+        let messages = rooms[roomID].messages.push(messageObject)
+
+        io.to(roomID).emit('MESSAGES', messageObject)
+    })
+
+    io.on('disconnect', (data) => {
+        let roomID
         let users = []
-        if (rooms[roomID] != undefined) {
-            users = rooms[roomID]['users']
-            users.forEach((user, index) => {
-                if (Object.keys(user)[0] == io.id) {
-                    users.splice(index, 1);
+        console.log('id: ', Object.keys(rooms))
+        Object.keys(rooms).forEach((room) => {
+            rooms[room].users.forEach((user) => {
+                if (Object.keys(user) == io.id) {
+                    roomID = room;
+                    users = rooms[roomID]['users']
+                    users.forEach((user, index) => {
+                        if (Object.keys(user)[0] == io.id) {
+                            rooms[roomID]['users'].splice(index, 1);
+                        }
+                    });
                 }
-            });
-            io.to(roomID).broadcast.emit(('USERS', users))
+            })
+        })
+        try {
+
+            io.to(roomID).broadcast.emit('USERS', rooms[roomID]['users'])
+        } catch (error) {
+
         }
+
+
     })
 })
 
@@ -58,14 +87,16 @@ app.use(express.json())
 
 app.get('/rooms/:id', (req, res) => {
     roomId = req.params.id
-    if(rooms[roomId]){
+    console.log(roomId)
+    if (rooms[roomId]) {
         res.send(rooms[roomId])
-    }else{
+    } else {
         res.send('New room')
     }
-    
+
 });
 app.get('/rooms', (req, res) => {
+
     res.send(rooms)
 });
 
